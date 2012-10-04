3ccd016fe78600c6ad47dd39273765f116a941b3
Ruben Mejias <rmejiascalderon@gmail.com>
2012-10-04 21:10:52 +0200

    Structura base

---
+++ src/js/html5.js
@@ -0,0 +1,10 @@
+document.createElement('aside');
+document.createElement('article');
+document.createElement('figure');
+document.createElement('figcaption');
+document.createElement('footer');
+document.createElement('header');
+document.createElement('hgroup');
+document.createElement('menu');
+document.createElement('nav');
+document.createElement('section');
