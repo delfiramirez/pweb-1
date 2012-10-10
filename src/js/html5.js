ffb85261ba4d7ec2438a1774dcc8c55d88b59cc9
delfiramirez <user@user.com>
2012-10-10 06:52:37 +0200

    Template enhancements 3
    1. Validate source
    2. Elemnts added to basic js file

--- src/js/html5.js
+++ src/js/html5.js
@@ -1,5 +1,6 @@
 document.createElement('aside');
 document.createElement('article');
+document.createElement('command');
 document.createElement('figure');
 document.createElement('figcaption');
 document.createElement('footer');
@@ -7,5 +8,8 @@ document.createElement('header');
 document.createElement('hgroup');
 document.createElement('menu');
 document.createElement('nav');
+document.createElement('picture');
 document.createElement('section');
+document.createElement('time');
+
 
