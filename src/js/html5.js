54ca9f2d4c236cd2e3f36853d48fc72d83e69ed2
delfiramirez <user@user.com>
2012-10-14 11:38:07 +0200

    HTML5 tables
    1. create Modal window with table displaying data
    2. TODO : responsive Table

--- src/js/html5.js
+++ src/js/html5.js
@@ -196,3 +196,22 @@ function removeEventSimple(obj,evt,fn) {
   else if (obj.detachEvent)
     obj.detachEvent('on'+evt,fn);
 }
+
+  // ref: http://diveintohtml5.org/detect.html
+  
+  function supports_input_placeholder()
+  {
+    var i = document.createElement('input');
+    return 'placeholder' in i;
+  }
+
+  if(!supports_input_placeholder()) {
+    var fields = document.getElementsByTagName('INPUT');
+    for(var i=0; i < fields.length; i++) {
+      if(fields[i].hasAttribute('placeholder')) {
+        fields[i].defaultValue = fields[i].getAttribute('placeholder');
+        fields[i].onfocus = function() { if(this.value == this.defaultValue) this.value = ''; }
+        fields[i].onblur = function() { if(this.value == '') this.value = this.defaultValue; }
+      }
+    }
+  }
