de6dbd0817997176f233500b348ceaf5e8017cb8
delfiramirez <user@user.com>
2012-10-15 17:46:12 +0200

    SRC minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 /* TODO
 
 call external UI JQuery ( if needed ): polyfill
-
 call Modernizr
-
 call CSS3.js
+call https://github.com/douglascrockford/JSON-js/blob/master/json2.js
 
 Build app flow
 
