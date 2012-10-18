a2107dd5289c461f52a36466a9223e64ffca9316
delfiramirez <user@user.com>
2012-10-18 17:32:23 +0200

    Extend
    1. HTML5: Local cache : adde manifest file appcache
    1.1. TODO: Declare link in HTML head
    2. JAVASCRIPT: Added JQuery dummie blank file.
    2.1.: removed cufon.js to be called asyncronous
    3. API : added Google JSAPI

---
+++ src/js/corporateQuery.js
@@ -0,0 +1,19 @@
+//* JQuery methods used in the project */
+
+/**
+
+A: All the content will be loaded via ajax /jsonp
+
+b: Use of JQuery as a library
+
+c: Try look up to  other libraries 
+
+**/
+
+//Simple test to validate contact form 
+
+//Simple call to lad and detect registerd users
+
+//Simple request to get external news feed
+
+//Triple request to l<oad multimedia data on select/ request
--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,17 @@ call external UI JQuery ( if needed ): polyfill
 call Modernizr
 call CSS3.js
 call https://github.com/douglascrockford/JSON-js/blob/master/json2.js
-
+call JQuery
 Build app flow
 
 
 et....
 
+TRY: use Google JSAPI to load external CDN APIS from Google
+
 */
 
+//Modernizr detection
 
 function detectCompatibilities() {
 
@@ -23,7 +26,6 @@ if (Modernizr.draganddrop) {
 }
 
 }
-
 
 
   // 
