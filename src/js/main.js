0650c6d83bc6d210c3da94837fdf6b59500a80f8
delfiramirez <user@user.com>
2012-10-29 11:16:01 +0100

    main.js : Appcache added
    1. Appcache testing and deploy

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,6 @@
 /* TODO
 
 
-/* deprecated : TODO create own detection in main.js
-call external UI JQuery ( if needed ): polyfill
-call Modernizr
-
-
-
 call CSS3.js
 call https://github.com/douglascrockford/JSON-js/blob/master/json2.js
 call JQuery
@@ -19,6 +13,64 @@ TRY: use Google JSAPI to load external CDN APIS from Google
 
 */
 
+/* Appcacache --------------------------------------------------------*/
+
+/*detect */
+if (window.applicationCache) {
+  var appCache = window.applicationCache;
+   appCache.addEventListener('error', appCacheError, false);
+   appCache.addEventListener('checking', checkingEvent, false);
+   appCache.addEventListener('noupdate', noUpdateEvent, false);
+   appCache.addEventListener('downloading', downloadingEvent, false);
+   appCache.addEventListener('progress', progressEvent, false);
+   appCache.addEventListener('updateready', updateReadyEvent, false);
+   appCache.addEventListener('cached', cachedEvent, false);
+}
+
+var messageCacheStatus = "Not supported";
+if (window.applicationCache) 
+{
+   var meuAppCache = window.applicationCache;
+   switch ( meuAppCache.status ) 
+   {
+      case meuAppCache.UNCACHED : 
+         messageCacheStatus = "Not cached"; 
+         break;
+      case meuAppCache.IDLE : 
+         messageCacheStatus = "Idle"; 
+         break;
+      case meuAppCache.CHECKING : 
+         messageCacheStatus = "Checking"; 
+         break;
+      case meuAppCache.DOWNLOADING : 
+         messageCacheStatus = "Downloading"; 
+         break;
+      case meuAppCache.UPDATEREADY : 
+         messageCacheStatus = "Update ready"; 
+         break;
+      case meuAppCache.OBSOLETE : 
+         messageCacheStatus = "Obsolete"; 
+         break;
+      default : 
+        messageCacheStatus = "Unexpected Status ( " + 
+                       meuAppCache.status.toString() + ")";
+        break;
+   }
+}
+return messageCacheStatus;
+
+  // _______________________________________________________________________________________
+//Appcache
+
+window.applicationCache.addEventListener('updateready', function(e) {
+  if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+    window.applicationCache.swapCache();
+    if (confirm('An updated version of this site is available.')) {
+      window.location.reload();
+    }
+  }
+}, false);
+ 
 
 
 function ourLocation(){
@@ -37,5 +89,17 @@ function loadLocation(){
 }
 
 
-  // 
-  // 
+
+/* xhr send data for Forms and JSON 29 Oct 2012 */
+
+function sendForm() {
+  var formData = new FormData();
+  formData.append('username', 'johndoe');
+  formData.append('id', 123456);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/server', true);
+  xhr.onload = function(e) { ... };
+
+  xhr.send(formData);
+}
